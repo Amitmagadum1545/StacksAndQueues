6d7ab100676bea45edddc2707514093cbac98305
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-25 23:38:04 +0530

    [Amit] Created Peak And Pop

--- StacksAndQueues/Stack.cs
+++ StacksAndQueues/Stack.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LinkedList;
-//using StacksAndQueues.LinkedList;
 
 namespace StacksAndQueues
 {
@@ -23,7 +22,7 @@ namespace StacksAndQueues
             if (this.top != null)
             {
                 newNode.next = this.top;       //newNode will hold address of top element
-                
+
             }
             this.top = newNode;                //top will point to new element
             Console.WriteLine("{0} is added to stack", newNode.data);
@@ -45,12 +44,41 @@ namespace StacksAndQueues
             else
             {
                 Node temp = this.top;
-                while (temp !=  null)
+                while (temp != null)
                 {
-                    Console.WriteLine("Value in the stack is: "+ temp.data);
+                    Console.WriteLine("Value in the stack is: " + temp.data);
                     temp = temp.next;
                 }
             }
+        }
+
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Console.WriteLine("{0} is top element of the stack", this.top.data);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("{0} is deleted from top of the stack", this.top.data);
+            this.top = this.top.next;
+        }
+        public bool IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+            return true;
         }
     }
 }
