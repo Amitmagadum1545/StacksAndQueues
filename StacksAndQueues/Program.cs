782758b2910ad480ae116a48fd952e607590777a
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-25 21:25:20 +0530

    [Amit] Added Welcome Message

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stacks And Queues");
+            Console.ReadLine(); 
+        }
+    }
+}
